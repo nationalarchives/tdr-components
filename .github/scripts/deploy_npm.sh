d53993f73b45c7d69e98a27f5fc98a1efe77fe7f
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-10-25 17:17:42 +0100

    Push after changes

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -6,6 +6,7 @@ cd package
 npm version patch
 git add package.json
 git commit -m 'Update npm version'
+git push
 npm publish --access public
 cd ..
 echo "set-npm-version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package/package.json)" >> $GITHUB_OUTPUT
