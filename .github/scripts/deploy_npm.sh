20acf6111f51572718763023519fb9059f63e014
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-10-25 17:00:04 +0100

    Fix the paths

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -2,7 +2,8 @@
 npm config set //registry.npmjs.org/:_authToken=$1
 npm ci
 npm run build
+cd package
 npm version patch
 npm publish --access public
 cd ..
-echo ::set-output name=set-npm-version::$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' ts/package.json)
+echo ::set-output name=set-npm-version::$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package.json)
