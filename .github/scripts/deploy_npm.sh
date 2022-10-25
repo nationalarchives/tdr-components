87286ecb7fd596f21f59485b2f2290b85d2a0321
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-10-25 17:09:39 +0100

    More git changes to the deploy job

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -4,6 +4,8 @@ npm ci
 npm run build
 cd package
 npm version patch
+git add package.json package-lock.json
+git commit -m 'Update npm version'
 npm publish --access public
 cd ..
-echo ::set-output name=set-npm-version::$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package.json)
+echo "set-npm-version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package/package.json)" >> $GITHUB_OUTPUT
