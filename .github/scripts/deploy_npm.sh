74c05c1643e16d55ba4aebdcccc516d16b99918a
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-10-25 16:48:50 +0100

    Fix the deploy action

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -3,9 +3,6 @@ npm config set //registry.npmjs.org/:_authToken=$1
 npm ci
 npm run build
 npm version patch
-git add package.json package-lock.json
-git commit -m 'Update npm version'
-git push
 npm publish --access public
 cd ..
 echo ::set-output name=set-npm-version::$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' ts/package.json)
