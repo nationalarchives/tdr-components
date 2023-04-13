4432803b3f74d4147ca2dddb79e62f8a4bfeaf72
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2023-04-13 15:36:00 +0100

    Adding disabled button component and documentation page

---
+++ src/nationalarchives/components/button-disabled/button-disabled.ts
@@ -0,0 +1,17 @@
+export class ButtonDisabled {
+  private readonly button : HTMLElement;
+
+  constructor(button: HTMLElement) {
+    this.button = button;
+  }
+
+  initialiseListeners: () => void = () => {
+    this.button.addEventListener("click", this.handleClick);
+  }
+
+  handleClick: (ev : Event) => void = (ev) => {
+    if(this.button.getAttribute('aria-disabled') == "true"){
+      ev.preventDefault();
+    }
+  }
+}
