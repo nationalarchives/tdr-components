2ab215b1cd4045f4570148ba8944ee665aa98ece
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2023-04-14 16:37:05 +0100

    Adding multiple options for displaying header

--- src/nationalarchives/components/header/header.stories.ts
+++ src/nationalarchives/components/header/header.stories.ts
@@ -1,5 +1,6 @@
 import "./_index.scss";
 import render from "./story.njk";
+import { Header } from "govuk-frontend";
 
 export default {
   title: "TDR/Header",
@@ -7,27 +8,56 @@ export default {
     layout: "fullscreen",
   },
   args: {
-    showSignOut: false,
+    isSignedIn: false,
   },
   argTypes: {
-    showSignOut: {
-      description: "Is the header showing the sign-out link?",
+    isSignedIn: {
+      description: "Is the header showing the signed-in menu items?",
     },
   },
+  decorators: [
+    (storyFn) => {
+      // Create an element that centres element with margin.
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("js-enabled");
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(storyFn(), "text/html");
+      wrapper.append(...doc.body.children);
+
+      var $header = wrapper.querySelector('[data-module="govuk-header"]');
+      if ($header) {
+        new Header($header).init();
+      }
+
+      return wrapper;
+    },
+  ],
 };
 
-const createCard = (args) => {
+const createHeader = (args) => {
   return render({
     params: { ...args },
   });
 };
 
 const Template = ({ label, ...args }) => {
-  return createCard({ label, ...args });
+  return createHeader({ label, ...args });
 };
 
 export const Default = Template.bind({});
 export const SignedIn = Template.bind({});
 SignedIn.args = {
-  showSignOut: true,
+  isSignedIn: true,
+};
+
+export const WithHiddenMenu = Template.bind({});
+WithHiddenMenu.args = {
+  isSignedIn: true,
+  withHiddenMenu: true,
+};
+WithHiddenMenu.argTypes = {
+  withHiddenMenu: {
+    description:
+      "If there is more than one menu item, use the hidden menu feature",
+  },
 };
