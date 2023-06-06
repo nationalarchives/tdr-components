b7a5edd740bc3e546e1ce0ac52ebdcd19b636d8d
vimleshg <vimlesh.gupta@nationalarchives.gov.uk>
2023-06-06 13:18:51 +0100

    ButtonDisabled component was missing in the npm package Add it in the index.ts to export

--- src/nationalarchives/index.ts
+++ src/nationalarchives/index.ts
@@ -1,2 +1,3 @@
 export * from "./components/nested-navigation/nested-navigation";
 export { MultiSelectSearch } from "./components/multi-select-search/multi-select-search";
+export { ButtonDisabled } from './components/button-disabled/button-disabled'
