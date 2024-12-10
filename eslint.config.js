2c4e3a86bd132e3747f94ca89a02c95499bcb475
TomJKing <tom.king@nationalarchives.gov.uk>
2024-12-10 14:44:17 +0000

    Turn off safe assignment lint warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,22 @@
-const eslintConfigLove = require('eslint-config-love');
+// const eslintConfigLove = require('eslint-config-love');
+//
+// module.exports = [
+//     {
+//       ...eslintConfigLove,
+//       files: [ 'src/**/*.ts' ]
+//     }
+//   ]
 
-module.exports = [
-    {
-      ...eslintConfigLove,
-      files: [ 'src/**/*.ts' ]
-    }
-  ]
+module.exports = (async function config() {
+    const { default: eslintConfigLove } = await import('eslint-config-love')
+
+    return [
+        {
+            ...eslintConfigLove,
+            files: [ '**/*.ts' ],
+            rules: {
+                '@typescript-eslint/no-unsafe-assignment': 'off'
+            }
+        },
+    ]
+})()
