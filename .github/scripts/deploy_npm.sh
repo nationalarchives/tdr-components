f5114f55dc7bdd7ee84c0b802f64eb00be4eb15a
EnyoHA <93252496+EnyoHA@users.noreply.github.com>
2026-06-09 16:15:21 +0100

    extra action to pull out npm version before bash script

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -8,6 +8,6 @@ npm version patch
 git add package.json
 git commit -m 'Update npm version'
 git push
+echo "set-npm-version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package/package.json)" >> $GITHUB_OUTPUT
 npm publish --access public
 cd ..
-echo "set-npm-version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package/package.json)" >> $GITHUB_OUTPUT
