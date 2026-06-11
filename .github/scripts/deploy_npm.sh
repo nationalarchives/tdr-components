843263cb48807ce81886c371d88758fdf6e6d554
Enyo Humphrey-Ackumey <93252496+EnyoHA@users.noreply.github.com>
2026-06-11 11:08:40 +0100

    small change 'set -e' added at start of script (#308)
    * small change 'set -e' added at start of script
    
    * Added failure slack message
    
    * extra action to pull out npm version before bash script
    
    * changed error message
    
    * changes for github repo name
    
    * message changed
    
    * changed slack message tdr_github_actions version

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 npm config set //registry.npmjs.org/:_authToken=$1
 npm ci
 npm run build
@@ -7,6 +8,6 @@ npm version patch
 git add package.json
 git commit -m 'Update npm version'
 git push
+echo "set-npm-version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package/package.json)" >> $GITHUB_OUTPUT
 npm publish --access public
 cd ..
-echo "set-npm-version=$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' package/package.json)" >> $GITHUB_OUTPUT
